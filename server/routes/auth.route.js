f6d18ff01086bf1f6b4178194f6b1877d0cd961d
BisalDangol <message2bishal@outlook.com>
2025-02-23 14:43:11 +0545

    login and registation

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -27,14 +27,38 @@ router.post("/register", async (req, res) => {
 
         const jwtToken = jwtGenerator(newUser.rows[0].user_id);
         console.log("New user added:", newUser.rows[0]);
-        return res.status(201).json({ message: "User registered successfully", user: newUser.rows[0] });
-        // return res.json({ jwtToken });
-
-
+        return res.status(201).json({ message: "User registered successfully", user: newUser.rows[0], jwtToken });
 
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Server Error");
+    }
+});
+
+//login route
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await pool.query("select * from users where useremail = $1", [email]);
+        if (user.rows.length === 0) {
+            console.log("password or email is incorrect");
+            return res.status(401).json("password or email is incorrect");
+        }
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].userpassword)
+
+        if (!validPassword) {
+            console.log("credential does not match with our record");
+            return res.status(401).json("credential does not match with our record");
+        }
+
+        const jwtToken = jwtGenerator(user.rows[0].user_id);
+        return res.status(201).json({ message: "User login successfully", user: user.rows[0], jwtToken });
+
+    } catch (error) {
+        console.error("Error:", error.message);
+        return res.status(500).json({ message: "Server Error" });
     }
 });
 
